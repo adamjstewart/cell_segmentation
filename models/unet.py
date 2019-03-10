783cc9491a27d9f09fac22be4d037546ff29e8e2
Adam J. Stewart <ajstewart426@gmail.com>
2019-03-09 20:51:23 -0600

    Simplify UNet model

--- models/unet.py
+++ models/unet.py
@@ -5,6 +5,15 @@ import torch.nn as nn
 __all__ = ['UNet', 'unet23']
 
 
+def double_conv(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, 3),
+        nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, 3),
+        nn.ReLU()
+    )
+
+
 def center_crop(img, output_size):
     _, _, h, w = img.size()
     _, _, th, tw = output_size
@@ -13,48 +22,69 @@ def center_crop(img, output_size):
     return img[:, :, i:i + th, j:j + tw]
 
 
+class Contract(nn.Module):
+
+    def __init__(self, in_channels, out_channels, dropout=False):
+        super(Contract, self).__init__()
+        assert in_channels < out_channels
+
+        self.pool = nn.MaxPool2d(2)
+        self.conv = double_conv(in_channels, out_channels)
+        self.drop = None
+
+        if dropout:
+            self.drop = nn.Dropout2d()
+
+    def forward(self, x):
+        x = self.pool(x)
+        x = self.conv(x)
+
+        if self.drop is not None:
+            x = self.drop(x)
+
+        return x
+
+
+class Expand(nn.Module):
+
+    def __init__(self, in_channels, out_channels):
+        super(Expand, self).__init__()
+        assert in_channels > out_channels
+
+        self.upconv = nn.ConvTranspose2d(in_channels, out_channels, 2)
+        self.relu = nn.ReLU()
+        self.conv = double_conv(in_channels, out_channels)
+
+    def forward(self, x, out):
+        x = self.upconv(x)
+        x = self.relu(x)
+
+        out = center_crop(out, x.size())
+        x = torch.cat([out, x], 1)
+
+        x = self.conv(x)
+
+        return x
+
+
 class UNet(nn.Module):
 
-    def __init__(self):
+    def __init__(self, in_channels=1):
         super(UNet, self).__init__()
 
-        self.relu = nn.ReLU()
-        self.pool = nn.MaxPool2d(2)
-        self.drop = nn.Dropout2d()
-
         # Contraction
-        self.conv1 = nn.Conv2d(1, 64, 3)
-        self.conv2 = nn.Conv2d(64, 64, 3)
-
-        self.conv3 = nn.Conv2d(64, 128, 3)
-        self.conv4 = nn.Conv2d(128, 128, 3)
-
-        self.conv5 = nn.Conv2d(128, 256, 3)
-        self.conv6 = nn.Conv2d(256, 256, 3)
-
-        self.conv7 = nn.Conv2d(256, 512, 3)
-        self.conv8 = nn.Conv2d(512, 512, 3)
-
-        self.conv9 = nn.Conv2d(512, 1024, 3)
-        self.conv10 = nn.Conv2d(1024, 1024, 3)
+        self.conv1_2 = double_conv(in_channels, 2 ** 6)
+        self.conv3_4 = Contract(2 ** 6, 2 ** 7)
+        self.conv5_6 = Contract(2 ** 7, 2 ** 8)
+        self.conv7_8 = Contract(2 ** 8, 2 ** 9, dropout=True)
+        self.conv9_10 = Contract(2 ** 9, 2 ** 10, dropout=True)
 
         # Expansion
-        self.conv11 = nn.ConvTranspose2d(1024, 512, 2)
-        self.conv12 = nn.Conv2d(1024, 512, 3)
-        self.conv13 = nn.Conv2d(512, 512, 3)
-
-        self.conv14 = nn.ConvTranspose2d(512, 256, 2)
-        self.conv15 = nn.Conv2d(512, 256, 3)
-        self.conv16 = nn.Conv2d(256, 256, 3)
-
-        self.conv17 = nn.ConvTranspose2d(256, 128, 2)
-        self.conv18 = nn.Conv2d(256, 128, 3)
-        self.conv19 = nn.Conv2d(128, 128, 3)
-
-        self.conv20 = nn.ConvTranspose2d(128, 64, 2)
-        self.conv21 = nn.Conv2d(128, 64, 3)
-        self.conv22 = nn.Conv2d(64, 64, 3)
-        self.conv23 = nn.Conv2d(64, 1, 1)
+        self.conv11_13 = Expand(2 ** 10, 2 ** 9)
+        self.conv14_16 = Expand(2 ** 9, 2 ** 8)
+        self.conv17_19 = Expand(2 ** 8, 2 ** 7)
+        self.conv20_22 = Expand(2 ** 7, 2 ** 6)
+        self.conv23 = nn.Conv2d(2 ** 6, 1, 1)
 
         # Initialize weights
         for m in self.modules():
@@ -64,74 +94,17 @@ class UNet(nn.Module):
 
     def forward(self, x):
         # Contraction
-        x = self.conv1(x)
-        x = self.relu(x)
-        x = self.conv2(x)
-        out1 = self.relu(x)
-
-        x = self.pool(out1)
-        x = self.conv3(x)
-        x = self.relu(x)
-        x = self.conv4(x)
-        out2 = self.relu(x)
-
-        x = self.pool(out2)
-        x = self.conv5(x)
-        x = self.relu(x)
-        x = self.conv6(x)
-        out3 = self.relu(x)
-
-        x = self.pool(out3)
-        x = self.conv7(x)
-        x = self.relu(x)
-        x = self.conv8(x)
-        x = self.relu(x)
-        out4 = self.drop(x)
-
-        x = self.pool(out4)
-        x = self.conv9(x)
-        x = self.relu(x)
-        x = self.conv10(x)
-        x = self.relu(x)
-        x = self.drop(x)
+        out1 = self.conv1_2(x)
+        out2 = self.conv3_4(out1)
+        out3 = self.conv5_6(out2)
+        out4 = self.conv7_8(out3)
+        x = self.conv9_10(out4)
 
         # Expansion
-        x = self.conv11(x)
-        x = self.relu(x)
-        out4 = center_crop(out4, x.size())
-        x = torch.cat([out4, x], 1)
-        x = self.conv12(x)
-        x = self.relu(x)
-        x = self.conv13(x)
-        x = self.relu(x)
-
-        x = self.conv14(x)
-        x = self.relu(x)
-        out3 = center_crop(out3, x.size())
-        x = torch.cat([out3, x], 1)
-        x = self.conv15(x)
-        x = self.relu(x)
-        x = self.conv16(x)
-        x = self.relu(x)
-
-        x = self.conv17(x)
-        x = self.relu(x)
-        out2 = center_crop(out2, x.size())
-        x = torch.cat([out2, x], 1)
-        x = self.conv18(x)
-        x = self.relu(x)
-        x = self.conv19(x)
-        x = self.relu(x)
-
-        x = self.conv20(x)
-        x = self.relu(x)
-        out1 = center_crop(out1, x.size())
-        x = torch.cat([out1, x], 1)
-        x = self.conv21(x)
-        x = self.relu(x)
-        x = self.conv22(x)
-        x = self.relu(x)
-
+        x = self.conv11_13(x, out4)
+        x = self.conv14_16(x, out3)
+        x = self.conv17_19(x, out2)
+        x = self.conv20_22(x, out1)
         x = self.conv23(x)
 
 
