35e8d28f706b8a3a9c5faab18f8e48eac516d2fc
Adam J. Stewart <ajstewart426@gmail.com>
2019-05-06 20:17:40 -0500

    Add median stats

--- calculate_statistics.py
+++ calculate_statistics.py
@@ -9,18 +9,22 @@ from datasets.slam import SLAM
 dataset = SLAM(os.path.join('data', 'SLAM'), train=True)
 
 running_mean = np.zeros(4)
+running_median = np.zeros(4)
 running_std = np.zeros(4)
 running_weight = np.zeros(2)
 
 for data, target in dataset:
     running_mean += np.mean(data, (0, 1))
+    running_median += np.median(data, (0, 1))
     running_std += np.std(data, (0, 1))
     running_weight += np.bincount(target.flatten()) / target.size
 
 mean = running_mean / len(dataset)
+median = running_median / len(dataset)
 std = running_std / len(dataset)
 weight = 1 - running_weight / len(dataset)
 
 print('mean=[{}, {}, {}, {}],'.format(*mean))
+print('median=[{}, {}, {}, {}],'.format(*median))
 print('std=[{}, {}, {}, {}]'.format(*std))
 print('weight=torch.Tensor([{}, {}])'.format(*weight))
